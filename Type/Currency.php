6507dc9262848a1b54eb4a082fcdd878c26f55c6
baks.dev <admin@baks.dev>
2023-01-25 16:57:26 +0300

    Baks Development

---
+++ Type/Currency.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ *  Copyright 2023.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+namespace BaksDev\Reference\Currency\Type;
+
+/** Валюта */
+final class Currency
+{
+    
+    public const TYPE = 'currency_type';
+
+    private CurrencyEnum $currency;
+    
+
+    public function __construct(string|CurrencyEnum $currency = null)
+    {
+        if($currency === null)
+        {
+            $currency = CurrencyEnum::DEFAULT;
+        }
+        
+        $this->currency = $currency instanceof CurrencyEnum ? $currency : CurrencyEnum::from($currency);
+    }
+
+    public function __toString() : string
+    {
+        return $this->currency->value;
+    }
+	
+    public function getValue() : string
+    {
+        return $this->currency->value;
+    }
+    
+
+    public function getName() : string
+    {
+        return $this->currency->name;
+    }
+    
+
+    public function getCurrency() : CurrencyEnum
+    {
+        return $this->currency;
+    }
+	
+
+    public static function cases() : array
+    {
+        $case = null;
+        
+        foreach(CurrencyEnum::cases() as $local)
+        {
+            $case[] = new self($local);
+        }
+        
+        return $case;
+    }
+
+}
