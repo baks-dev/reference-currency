c77acd50993b0cc3ea9ff22e1342a310fac4cf13
kingstar <admin@baks.dev>
2024-02-08 00:51:15 +0300

    Baks Development

--- Type/CurrencyType.php
+++ Type/CurrencyType.php
@@ -25,15 +25,16 @@ namespace BaksDev\Reference\Currency\Type;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\StringType;
+use Doctrine\DBAL\Types\Type;
 
-final class CurrencyType extends StringType
+final class CurrencyType extends Type
 {
-	public function convertToDatabaseValue($value, AbstractPlatform $platform): mixed
+	public function convertToDatabaseValue($value, AbstractPlatform $platform): string
 	{
 		return (string) $value;
 	}
 	
-	public function convertToPHPValue($value, AbstractPlatform $platform): mixed
+	public function convertToPHPValue($value, AbstractPlatform $platform): ?Currency
 	{
         return !empty($value) ? new Currency($value) : null;
 	}
@@ -49,5 +50,10 @@ final class CurrencyType extends StringType
 	{
 		return true;
 	}
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        return $platform->getStringTypeDeclarationSQL($column);
+    }
 
 }
